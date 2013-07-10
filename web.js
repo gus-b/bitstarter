43959576127142c68098986e78df1c3c19039e8d
Gustavo Bonany <gustavopbo@gmail.com>
2013-07-10 00:38:05 +0000

    web.js from index

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var msg1 = new Buffer (fs.readfile ("index.html"));   
+  var msg1 = new Buffer (fs.readFileSync ("index.html"));   
   response.send(msg1.toString ());
 });
 
